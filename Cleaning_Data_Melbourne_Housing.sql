fa4f0b109f9b097cb3b0f2385acd89b8c1b82988
sonnige-nacht <iman.ahangariyan@gmail.com>
2025-12-30 17:43:32 +0330

    complete cleaning task

--- Cleaning_Data_Melbourne_Housing.sql
+++ Cleaning_Data_Melbourne_Housing.sql
@@ -29,24 +29,50 @@ HAVING CNT > 1;
 # Clean Address: the numbers have to be removed. (Example: '123/56 Nicholson St' → 'Nicholson St')
 UPDATE clean_data_melb_data
 SET clean_data_melb_data.Address = CASE
-    WHEN address LIKE '% %'
-    THEN SUBSTRING(clean_data_melb_data.Address, LOCATE(' ', clean_data_melb_data.Address) + 1)
-    ELSE address
-END;
+    WHEN Address LIKE '% %'
+    THEN SUBSTRING(Address, LOCATE(' ', Address) + 1)
+    END;
 
 # Clean Type: complete the data (Example: 'h' → 'House' or 'u' → 'unit')
 UPDATE clean_data_melb_data
-SET clean_data_melb_data.`Type` = REPLACE(clean_data_melb_data.`Type`, 'h', 'House')
-WHERE clean_data_melb_data.`Type` LIKE 'h';
+SET `Type` = REPLACE(`Type`, 'h', 'House')
+	WHERE `Type` LIKE 'h';
 
 UPDATE clean_data_melb_data
-SET clean_data_melb_data.`Type` = REPLACE(clean_data_melb_data.`Type`, 'u', 'Unit')
-WHERE clean_data_melb_data.`Type` LIKE 'u';
+SET `Type` = REPLACE(`Type`, 'u', 'Unit')
+WHERE `Type` LIKE 'u';
 
 UPDATE clean_data_melb_data
-SET clean_data_melb_data.`Type` = REPLACE(clean_data_melb_data.`Type`, 't', 'Townhouse')
-WHERE clean_data_melb_data.`Type` LIKE 't';
+SET `Type` = REPLACE(`Type`, 't', 'Townhouse')
+WHERE `Type` LIKE 't';
 
 UPDATE clean_data_melb_data
-SET clean_data_melb_data.`Type` = REPLACE(clean_data_melb_data.`Type`, 'res', 'Others')
-WHERE clean_data_melb_data.`Type` LIKE 'res';
+SET `Type` = REPLACE(`Type`, 'res', 'Others')
+WHERE `Type` LIKE 'res';
+
+# Drop some columns, which are not really important for analysis
+ALTER TABLE clean_data_melb_data
+DROP COLUMN Lattitude;
+
+ALTER TABLE clean_data_melb_data
+DROP COLUMN Longtitude;
+
+# standardize the 'Date' cloumn
+UPDATE clean_data_melb_data
+SET `Date` = STR_TO_DATE(`date`, '%d/%m/%Y');
+ALTER TABLE clean_data_melb_data
+MODIFY COLUMN `Date` DATE;
+
+# covert the records to NULL, where BuildingArea or YearBuilt are an empty string
+UPDATE clean_data_melb_data
+SET BuildingArea = NULL
+WHERE BuildingArea = '';
+UPDATE clean_data_melb_data
+SET YearBuilt = NULL
+WHERE YearBuilt = '';
+
+# standardize the 'BuildingArea' and 'YearBuilt' cloumn
+ALTER TABLE clean_data_melb_data
+MODIFY COLUMN BuildingArea INT;
+ALTER TABLE clean_data_melb_data
+MODIFY COLUMN YearBuilt INT;
